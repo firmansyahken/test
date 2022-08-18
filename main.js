60f991583576c5d77b3aa8e2626fc0f2647842a9
firmansyahken <firmansyahken4@gmail.com>
2022-08-18 15:57:32 +0700

    Updated All

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
-document.addEventListener( 'DOMContentLoaded', function() {
-  var splide = new Splide( '.splide' );
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide", {
+    type: "loop",
+    autoplay: true,
+    interval: 7000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
   splide.mount();
-} );
+});
 
-const preloader = document.querySelector(".preloader")
-const popupBtn = document.getElementById("popup_toggle")
-var navLink = document.querySelectorAll(".nav_link")
+const preloader = document.querySelector(".preloader");
+const popupBtn = document.getElementById("popup_toggle");
+// const navLink = document.querySelectorAll(".nav_link")
+// const navMenu = document.querySelectorAll(".nav_menu")
+// const navMobileContainer = document.querySelector(".navbar_mobile_container")
+// const navBtn = document.getElementById("navbar_toggle")
 
 setTimeout(() => {
-  preloader.classList.add("disable")
+  preloader.classList.add("disable");
   setTimeout(() => {
-    preloader.style.display = "none"
-  }, 100)
-}, 2000)
+    preloader.style.display = "none";
+  }, 100);
+}, 2000);
 
-popupBtn.addEventListener("click", function() {
-  document.querySelector(".popup").style.display = "none"
-  document.body.style.overflow = "scroll"
-})
+// navBtn.addEventListener("click", function() {
+//   navMobileContainer.classList.toggle("active")
+//   navMobileContainer.classList.contains("active") ? navBtn.setAttribute("class", "fa fa-times") : 
+//   navBtn.setAttribute("class", "fa fa-bars")
+// })
 
-navLink.forEach(link => {
-  link.addEventListener("click", function() {
-    document.getElementById(this.dataset.id).classList.toggle("active")
-  })
-})
+popupBtn.addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+  document.body.style.overflow = "scroll";
+});
+
+// navLink.forEach(link => {
+//   link.addEventListener("click", function() {
+//     let linkActive = document.getElementById(this.dataset.id)
+//     if(linkActive.classList.contains("active")) {
+//       navMenu.forEach(menu => { menu.classList.remove("active") })
+//       return linkActive.classList.remove("active")
+//     } 
+    
+//     navMenu.forEach(menu => { menu.classList.remove("active") })
+//     return linkActive.classList.add("active")
+
+//   })
+// })
